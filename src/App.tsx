9751d8c4628c9ee0a36bc6660cb98bdbafe897c0
samanthathompson52 <samanthathompson521@gmail.com>
2023-09-26 17:06:59 -0400

    Started creation and design layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Home from './Home.jsx';
 import './App.css';
 
+function getYear() {
+  return new Date().getFullYear();
+}
+
 function App() {
+
+  const [Loading, setLoading] = useState(true);
+
+
+  setTimeout(()=>{
+      setLoading(false);
+    }, 2000
+  );
   return (
-    <div>
-      Hello World!
+    <div>    
+      <div className={Loading ? 'App overflow-hidden' : 'App'}>
+        
+        {/* <Loader Loading={Loading} /> */}
+        {/* <Header /> */}
+        {/* <Links /> */}
+        {/* <AboutMe />
+        <Projects />
+        <Experience /> */}
+        {/* <Contact />  */}
+        {/* <Footer /> */}
+        
+      </div>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#about">About</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    <Home />
+
+    <section id="about">
+        <div className="content">
+            <h2>About Me</h2>
+            <p>Add your about me content here.</p>
+        </div>
+    </section>
+
+    <section id="portfolio" className="parallax">
+        <div className="content">
+            <h2>Portfolio</h2>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div className="content">
+            <h2>Contact Me</h2>
+        </div>
+    </section>
+
+    <footer>
+        <p>&copy; <span>{getYear()}</span> Samantha Thompson</p>
+    </footer>
     </div>
   );
 }
---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+        <div className = "landing-page">
+              <div className = "p-4">   
+                <h1>Samantha Thompson</h1>
+                <h2>Full Stack Software Developer</h2>
+                <h3 className = "py-3">Learning about the power of technology and the impact it can have on the world.</h3>
+              </div>
+              <span id= 'about'></span>
+        </div> 
+    </div>
+  );
+}
+
+export default Home;
