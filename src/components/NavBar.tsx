e2afe50895b3750ad46f8da713ed135563214f42
samanthathompson52 <samanthathompson521@gmail.com>
2023-10-03 20:52:51 -0400

    Added hover effect on NavBar links

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -33,6 +33,7 @@ export default function Example() {
                       <a
                         key={item.name}
                         href={item.href}
+                        className="transition duration-300 ease-in-out hover:scale-110"
                       >
                         {item.name}
                       </a>
