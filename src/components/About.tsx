f0b1ea094144cc06d327bd6f65613d830a3889e3
samanthathompson52 <samanthathompson521@gmail.com>
2023-10-24 21:15:32 -0400

    Updated resume and about me section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,9 +15,11 @@ const About = () => {
                 <img src={wave} className="wave" alt=""></img>
               </div>
               <h1 className = "md:text-5xl text-4xl bold mb-5">About Me</h1>
-              <p>Neurobiology & Physiology graduate turned software developer.<br></br><br></br>Throughout my undergraduate education I found myself gaining new experiences in clinical, research, and academia areas of biology. During this, I realized how much of an impact technology can have on healthcare and research!
-I discovered that I loved programming and began teaching myself everything I could! <br></br> <br></br>Since graduation, I dedicated my time to learning even more about the growing technologies of the world.
-Now, I wish to create something of my own and learn even more aspects of technology.</p>
+              <p>
+                Neurobiology & Physiology graduate turned Software Developer.
+                <br></br><br></br>
+                Throughout my undergraduate education, I gained new experiences in clinical, research, and academic areas of biology. During this I realized how much of an impact technology can have on healthcare and research! I discovered I loved programming through several of Purdue University's computer science courses and now I continue to dedicate my time to learning even more about the growing technologies of the world!               
+              </p>
             </div>
           </div>
       </div> 
