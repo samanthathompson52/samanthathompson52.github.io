904f9898677d26712753ea609b9071767e78dc7c
samanthathompson52 <samanthathompson521@gmail.com>
2024-02-20 21:51:16 -0500

    Update resume and project information

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
             <div className = "self-center md:pl-20 pl-5">   
               <h1 className = "md:text-7xl text-5xl font-bold pb-2">Samantha Thompson</h1>
               <img src={helloImage} className = "hello-image animate-pulse" alt=""></img>
-              <h2 className = "text-main text-3xl">Full Stack Software Developer</h2>
+              <h2 className = "text-main text-3xl">Software Developer</h2>
             </div>
             <span id= 'home'></span>
         </div> 
